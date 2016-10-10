4f3f1df1832ad456b46007c05fdc532e499bbd0f
Nolan Darilek <nolan@thewordnerd.info>
2016-10-10 16:45:58 -0500

    Add demo support and use more ES2015.

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-var express = require('express')
-var path = require('path')
-var logger = require('morgan')
-var bodyParser = require('body-parser')
+var express = require("express"),
+  path = require("path"),
+  logger = require("morgan")
 
-var routes = require('./routes')
+const routes = require("./routes")
 
-var app = express()
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "jade")
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use('/', routes)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found')
+app.use((req, res, next) => {
+  const err = new Error("Not Found")
   err.status = 404
   next(err)
 })
@@ -29,10 +26,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === 'development') {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500)
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     })
@@ -41,9 +38,9 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500)
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   })
--- routes.js
+++ routes.js
@@ -1,28 +1,22 @@
-var exec = require("child_process").exec
-var express = require('express')
-var router = express.Router()
-var gitBackend = require("git-http-backend")
+var exec = require("child_process").exec,
+  express = require('express'),
+  router = express.Router(),
+  gitBackend = require("git-http-backend")
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
-});
+router.get('/', (req, res) => res.render("index"));
 
 router.get("/publicId", (req, res) => {
   const sessionId = req.headers["x-sandstorm-session-id"]
   exec(`getPublicId ${sessionId}`, (err, rv) => {
-    if(err) {
-      return console.log(err)
-    }
+    if(err)
+      return res.end(err)
     const lines = rv.split("\n")
     const publicId = lines[0]
     const hostname = lines[1]
     const domain = publicId+"."+hostname
     const url = lines[2]
-    res.render("publicId", {
-      domain: domain,
-      publicId: publicId,
-      url: url
-    })
+    const isDemo = lines[3] == "true"
+    res.render("publicId", {domain, isDemo, publicId, url})
   })
 })
 
@@ -37,4 +31,4 @@ router.use("/git", (req, res) => {
   }).pipe(res)
 })
 
-module.exports = router;
+module.exports = router
